f15dcf016b22d3ea1796fb812d2db1f1272577ef
Ewen <ewengue1432@gmail.com>
2025-09-22 22:25:32 -0400

    canvas class

---
+++ backend/canvas.py
@@ -0,0 +1,14 @@
+class Canvas:
+    def __init__(self):
+        self.width = 100
+        self.height = 100
+    
+    def __init__(self, width, height):
+        self.width = 100
+        self.height = 100
+
+    def __private():
+        pass
+
+    def _protected():
+        pass
